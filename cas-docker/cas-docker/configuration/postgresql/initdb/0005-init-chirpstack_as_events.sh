c3bb24c639abdfc6364c4affd384fd1db7dcf18d
aayafrontenda <eeaayeyeah@gmail.com>
2024-05-17 12:53:32 +0500

    feat: remove same nested folder

--- cas-docker/cas-docker/configuration/postgresql/initdb/0005-init-chirpstack_as_events.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create role chirpstack_as_events with login password 'dbpassword';
-    create database chirpstack_as_events with owner chirpstack_as_events;
-EOSQL
